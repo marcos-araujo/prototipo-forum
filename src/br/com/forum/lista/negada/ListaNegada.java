48a8cae102b504b92d4898ca11561a2feaf598ee
marcos.txt@gmail.com <marcos.txt@gmail.com>
2020-07-30 23:56:37 -0300

    Refactoring blacklist logic

--- src/br/com/forum/lista/negada/ListaNegada.java
+++ src/br/com/forum/lista/negada/ListaNegada.java
@@ -11,25 +11,22 @@ import br.com.forum.model.PalavraNegada;
 
 public class ListaNegada{
 
-	private PalavraNegadaDAO palavraDAO;
-	private ArrayList<PalavraNegada> listaPalavra;
-
-	//Lógica de validação dos comentários contra a black list 
 	public String verificaListaNegada(String texto, Connection connection) throws SQLException{
 		
-		palavraDAO = new PalavraNegadaDAO(connection);
+		PalavraNegadaDAO palavraDAO = new PalavraNegadaDAO(connection);
 		
-		listaPalavra = palavraDAO.lista();
+		ArrayList<PalavraNegada> listaPalavra = palavraDAO.lista();
 		
-		for(int x=0; x<listaPalavra.size(); x++){
-			//Verificar Possibilidades
-			String regex = "(?i)" + listaPalavra.get(x).getPalavra().replace("a", "(a|@|ã|á|à|â)").replace("i", "(i|1|í|ì)").replace("o", "(o|0|ó|ò|ô|õ)").replace("s", "(s|&)");
+		for (PalavraNegada palavraNegada : listaPalavra) {
+			
+			String regex = "(?i)" + palavraNegada.getPalavra().replace("a", "(a|@|ã|á|à|â)").replace("i", "(i|1|í|ì)").replace("o", "(o|0|ó|ò|ô|õ)").replace("s", "(s|&)");
 			Pattern p = Pattern.compile(regex);
 			Matcher m = p.matcher(texto);
             if(m.find())
             	texto = texto.replaceAll(m.group(), m.group().replaceAll(".", "x"));
 		}
 		return texto;
+		
 	}
 
 }
