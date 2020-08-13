7dae9c4bdd4f7da05d317d4df079297b615bc229
marcos.txt@gmail.com <marcos.txt@gmail.com>
2020-08-13 20:34:33 -0300

    Branch para deploy no heroku

--- src/br/com/forum/jdbc/ConnectionFactory.java
+++ src/br/com/forum/jdbc/ConnectionFactory.java
@@ -1,11 +1,11 @@
 package br.com.forum.jdbc;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,12 +15,12 @@ public class ConnectionFactory {
     private static final Logger logger = LogManager.getLogger(ConnectionFactory.class);
 	
     public Connection getConnection() throws URISyntaxException, IOException{
-    	Properties properties = new Properties();
-    	properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
-    	String forname	= properties.get("database.server.forname").toString();
-    	String username = properties.get("database.server.user").toString();
-    	String password = properties.get("database.server.password").toString();
-    	String url = properties.get("database.server.url").toString();
+    	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+    	String forname = "org.postgresql.Driver";
+    	String username = dbUri.getUserInfo().split(":")[0];
+    	String password = dbUri.getUserInfo().split(":")[1];
+    	String url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
         
 		try{
 			Class.forName(forname);
