ff4460eb465b5b05975549712ac0e4ebf395474e
Marcos Araujo <marcos.txt@gmail.com>
2020-09-04 23:17:20 -0300

    heroku configo=

--- src/br/com/forum/jdbc/ConnectionFactory.java
+++ src/br/com/forum/jdbc/ConnectionFactory.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,23 +16,40 @@ public class ConnectionFactory {
     private static final Logger logger = LogManager.getLogger(ConnectionFactory.class);
 	
     public Connection getConnection() throws URISyntaxException, IOException{
-    	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		String username;
+		String forname;
+		String password;
+		String url;
 
-    	String forname = "org.postgresql.Driver";
-    	String username = dbUri.getUserInfo().split(":")[0];
-    	String password = dbUri.getUserInfo().split(":")[1];
-    	String url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-        
-		try{
+		try {
+
+			if (System.getenv("DATABASE_URL") != null) {
+				URI dbUri = new URI(System.getenv("DATABASE_URL"));
+				forname = "org.postgresql.Driver";
+				username = dbUri.getUserInfo().split(":")[0];
+				password = dbUri.getUserInfo().split(":")[1];
+				url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+			} else {
+				Properties properties = new Properties();
+				properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+				forname = properties.get("database.server.forname").toString();
+				username = properties.get("database.server.user").toString();
+				password = properties.get("database.server.password").toString();
+				url = properties.get("database.server.url").toString();
+			}
+		
 			Class.forName(forname);
 			return DriverManager.getConnection(url, username, password);
-		}catch(SQLException e){
+
+		} catch (SQLException e) {
 			logger.error(e);
 			throw new RuntimeException(e);
-		}catch(ClassNotFoundException e){
+		} catch (ClassNotFoundException e) {
 			logger.error(e);
 		}
+		
 		return null;
+
 	}
 
 }
